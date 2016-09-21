7ef251a18a6f34a001c3cfe7fad5bc126975637f
Michael Teter <michael.teter@gmail.com>
2016-09-22 01:25:54 +0200

    first commit

--- NavbarTransparent/ViewController.swift
+++ NavbarTransparent/ViewController.swift
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let nav = self.navigationController?.navigationBar {
+            nav.setBackgroundImage(UIImage(), for: .default)
+            nav.shadowImage = UIImage()
+            nav.isTranslucent = true
+        }
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# Transparent UINavigationBar Example
